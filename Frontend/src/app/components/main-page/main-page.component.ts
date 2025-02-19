a6899e2eec24bcadda76f377188250a59575f051
MszrsRicsi <78954828+MszrsRicsi@users.noreply.github.com>
2025-02-19 12:28:18 +0100

    Navbar - Frontend

--- Frontend/src/app/components/main-page/main-page.component.ts
+++ Frontend/src/app/components/main-page/main-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../navbar/navbar.component';
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss'
 })
--- Frontend/src/app/components/navbar/navbar.component.ts
+++ Frontend/src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
+
 export class NavbarComponent {
 
+  openSidebarNav()
+  {
+    document.querySelector(".sidebarNav")?.classList.add("open");
+    document.querySelector("body")!.style.overflow = "hidden";
+  }
+
+  closeSidebarNav()
+  {
+    document.querySelector(".sidebarNav")?.classList.remove("open");
+    document.querySelector("body")!.style.overflow = "auto";
+  }
 }
